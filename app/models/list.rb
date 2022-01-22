2e9386e39576e2aa66de03952b0e3e9bdfe1612c
Renee Godyn <renitatje@hotmail.com>
2022-01-22 14:41:27 +0000

    generated a static seed of movies to choose from

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  has_many :movies, through: :bookmarks
   has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
   validates :name, uniqueness: true, presence: true
 end
